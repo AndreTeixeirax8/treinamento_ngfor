0b46703f31bef115e6f3a5d430c63cda9b44354b
André Teixeira <andre-x8@hotmail.com>
2025-03-10 15:51:48 -0300

    adicionado o ng for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngFor';
+  listPessoas = [
+    {
+      name: 'Aluno_01',
+      idade: 26,
+    },
+    {
+      name: 'Aluno_02',
+      idade: 36,
+    },
+    {
+      name: 'Aluno_03',
+      idade: 46,
+    },
+    {
+      name: 'Aluno_04',
+      idade: 56,
+    },
+  ];
 }
